13d50fcda05f59f4ea32bf64beb109d19e0fdf20
intern.btnghia <tralfargar215@gmail.com>
2023-08-11 17:33:01 +0700

    tan lam

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
+import '../blocs/bloc_exports.dart';
+import '../models/task.dart';
 import '../widgets/task_list.dart';
 
 class TasksScreen extends StatelessWidget {
-   const TasksScreen({super.key});
+  const TasksScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Task App'),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Center(
-            child: Chip(label: Text('Tasks:')),
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        List<Task> tasksList = state.allTasks;
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Task App'),
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+            ],
           ),
-          TasksList(taskList:taskList)
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Add Task',
-        child: const Icon(Icons.add),
-      ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(
+                child: Chip(label: Text('Tasks:')),
+              ),
+              TasksList(taskList: taskList)
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Add Task',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 }
